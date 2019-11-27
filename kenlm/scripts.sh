51c52928fa153058d82a428b26c5385ca1f21347
dato <mysmilesh@gmail.com>
2019-11-27 13:28:48 +0900

    kenlm usage

---
+++ kenlm/scripts.sh
@@ -0,0 +1,9 @@
+# WIKI
+#./lmplz -o 4 -S 80% -T ./tmp <./corpus/enwiki_train-word_tokenize.txt_lo_tok >./model/4-enwiki.arpa
+#./build_binary ./model/2-enwiki.arpa ./model/2-enwiki.binary
+#./query ./model/2-enwiki.binary <./corpus/test.txt
+
+
+# libri
+#./lmplz -o 4 -S 80% -T ./tmp <./corpus/librispeech_train.txt_lo_tok >./model/4-libri.arpa
+#./build_binary ./model/2-libri.arpa ./model/2-libri.binary
